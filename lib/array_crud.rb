b30682b37861eef064c9fd33245f06a5bcbb85b1
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-07 17:58:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [birthday, age, gender, location]
+  ["Elle", "Moon", "Sophie", "Sydney"]
 end
 
 def add_element_to_end_of_array(array, element)
